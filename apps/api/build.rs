a955e53908586f6b14b8ce9a21c7f4ba7dec60dd
Ziad Alzarka <ziadalzarka@gmail.com>
2024-07-29 01:58:13 +0300

    chore(build): link whatsapp to api

--- apps/api/build.rs
+++ apps/api/build.rs
@@ -1,26 +1,43 @@
 use std::env;
 use std::path::PathBuf;
 
-struct WhatsAppLinkingInfo<'a> {
-    library_file_name: &'a str,
-    relative_path: &'a str,
-}
-
 #[cfg(all(target_arch = "aarch64", target_os = "macos"))]
-fn get_whatsapp_linking_info() -> WhatsAppLinkingInfo<'static> {
+fn link_whatsapp_library() {
     println!("cargo:warning=Compiling for Apple Silicon");
     println!("cargo:rustc-link-lib=framework=Security");
     println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET=11.0");
-    WhatsAppLinkingInfo {
-        library_file_name: "libwhatsapp.a",
-        relative_path: "./binaries/darwin-arm64",
+
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let relative_path: PathBuf = current_dir.join("./binaries/darwin-arm64");
+    let library_file_path = relative_path.join("libwhatsapp.a");
+
+    // Check if the file exists
+    if !library_file_path.exists() {
+        panic!(
+            "WhatsApp library file not built: {}",
+            library_file_path.display()
+        );
     }
+
+    let link_search_path = relative_path
+        .to_str()
+        .expect("Failed to convert path to string");
+
+    println!("cargo:rustc-link-search=native={}", link_search_path);
+    println!(
+        "cargo:warning=WhatsApp library search path: {}",
+        link_search_path
+    );
+    println!("cargo:rustc-link-lib=static=whatsapp");
 }
 
-fn link_whatsapp_library(whatsapp_linker_info: WhatsAppLinkingInfo) {
+#[cfg(all(target_arch = "x86_64", target_os = "windows"))]
+fn link_whatsapp_library() {
+    println!("cargo:warning=Compiling for Windows x86_64");
+
     let current_dir = env::current_dir().expect("Failed to get current directory");
-    let relative_path: PathBuf = current_dir.join(whatsapp_linker_info.relative_path);
-    let library_file_path = relative_path.join(whatsapp_linker_info.library_file_name);
+    let relative_path: PathBuf = current_dir.join("./binaries/windows-x86_64");
+    let library_file_path = relative_path.join("libwhatsapp.a");
 
     // Check if the file exists
     if !library_file_path.exists() {
@@ -43,5 +60,5 @@ fn link_whatsapp_library(whatsapp_linker_info: WhatsAppLinkingInfo) {
 }
 
 fn main() {
-    link_whatsapp_library(get_whatsapp_linking_info());
+    link_whatsapp_library();
 }
--- libs/whatsapp-lib/build.sh
+++ libs/whatsapp-lib/build.sh
@@ -4,7 +4,7 @@ echo "Running build script for WhatsApp Bot..."
 
 # Check if the argument is provided
 if [ -z "$1" ]; then
-  echo "Error: No argument provided. Please specify one of the following: darwin-arm64, windows-x86_64, windows-i686."
+  echo "Error: No argument provided. Please specify one of the following: darwin-arm64, windows-x86_64."
   exit 1
 fi
 
@@ -19,18 +19,9 @@ case $1 in
     echo "Building WhatsApp Bot for Windows x86_64..."
     mkdir -p target/windows-x86_64
     CGO_ENABLED=1 GOOS=windows GOARCH=amd64 go build -o target/windows-x86_64/libwhatsapp.a -buildmode=c-archive main.go
-    cd target/windows-x86_64
-    # Generate a .def file from MingW library (.a file)
-    dlltool -z whatsapplib.def --export-all-symbols libwhatsapp.a
-    # Generate a .lib file from the .def file
-    dlltool -m i386:x86-64 -d whatsapplib.def -l libwhatsapp.lib -A libwhatsapp.a
-    ;;
-  windows-i686)
-    echo "Building WhatsApp Bot for Windows i686..."
-    # Add your Windows i686 specific commands here
     ;;
   *)
-    echo "Error: Invalid argument. Please specify one of the following: macos-arm64, macos-amd64, windows-amd64, windows-i686."
+    echo "Error: Invalid argument. Please specify one of the following: darwn-arm64, windows-amd64."
     exit 1
     ;;
 esac
