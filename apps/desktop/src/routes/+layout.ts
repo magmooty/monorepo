ea2f2c0f6954e92ff5f26186f196b397c864703d
Ziad Alzarka <ziadalzarka@gmail.com>
2024-08-24 02:38:55 +0300

    chore: try logging in browser window

--- apps/desktop/src/routes/+layout.ts
+++ apps/desktop/src/routes/+layout.ts
@@ -1,2 +1,14 @@
 export const prerender = true;
 export const ssr = false;
+console.log = (...params) => {
+  document.body.innerText += `${JSON.stringify(params)}`;
+};
+console.error = (...params) => {
+  document.body.innerText += `${JSON.stringify(params)}`;
+};
+console.info = (...params) => {
+  document.body.innerText += `${JSON.stringify(params)}`;
+};
+console.debug = (...params) => {
+  document.body.innerText += `${JSON.stringify(params)}`;
+};
