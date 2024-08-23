b4b512c1961816fdd9a4f75423a1285187e14154
Ziad Alzarka <ziadalzarka@gmail.com>
2024-08-23 22:28:41 +0300

    fix: wait for connection in layout.svelte

--- apps/desktop/src/routes/+layout.svelte
+++ apps/desktop/src/routes/+layout.svelte
@@ -4,8 +4,15 @@
 	import { app } from 'sdk';
 	import '../i18n';
 
+	let connected = false;
+
 	// Initialization logic goes here
-	app.connect().then(closeSplashScreen);
+	app.connect().then(() => {
+		closeSplashScreen();
+		connected = true;
+	});
 </script>
 
-<slot></slot>
+{#if connected}
+	<slot></slot>
+{/if}
