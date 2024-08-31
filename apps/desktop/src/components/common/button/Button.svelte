9238a129cccd7eabe14b469180bce6cd9e194ce9
Mohab Akram <mohapmobile@gmail.com>
2024-08-31 17:11:17 +0300

    feat: add button component (#44)
    * feat: add button component
    
    * refactor classlist logic

---
+++ apps/desktop/src/components/common/button/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Button, Spinner } from 'flowbite-svelte';
+	export let variant: 'primary' | 'secondary' | 'link' | 'transparent' = 'primary';
+	export let loading: boolean = false;
+
+	let classList;
+
+	const isSecondary = variant === 'secondary';
+
+	if (variant === 'primary') {
+		classList = `hover:bg-primary bg-primary text-white`;
+	} else if (variant === 'secondary') {
+		classList = `text-black hover:text-back`;
+	} else if (variant === 'link') {
+		classList = `text-primary hover:text-primary-600 underline border-none outline-none`;
+	} else if (variant === 'transparent') {
+		classList = `text-primary bg-transparent border-none`;
+	}
+</script>
+
+<Button class={classList} on:click outline={isSecondary} {...$$restProps}>
+	{#if loading}
+		<Spinner class="me-3" size="4" color="white" />
+	{/if}
+	<slot />
+</Button>
--- apps/desktop/src/routes/+page.svelte
+++ apps/desktop/src/routes/+page.svelte
@@ -1,1 +1,51 @@
+<script>
+	import Button from 'components/common/button/Button.svelte';
+	import Locales from 'components/locales.svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+</script>
+
 <h1>Home</h1>
+
+<div class="m-4">
+	<h1>Default</h1>
+	<div class="align-center mt-5 flex gap-4">
+		<Button>Default</Button>
+		<Button variant="secondary">Secondary</Button>
+		<Button variant="transparent">Transparent</Button>
+		<Button variant="link">Link</Button>
+	</div>
+</div>
+
+<div class="m-4">
+	<h1>With Icons</h1>
+	<div class="align-center mt-6 flex gap-4">
+		<Button><ArrowRightOutline class="text-primary-700 h-7 w-7" />Default</Button>
+		<Button variant="secondary"
+			><ArrowRightOutline class="text-primary-700 h-7 w-7" />Secondary</Button
+		>
+		<Button variant="transparent">
+			<ArrowRightOutline class="text-primary-700 h-7 w-7" />Transparent</Button
+		>
+		<Button variant="link"><ArrowRightOutline class="text-primary-700 h-7 w-7" />Link</Button>
+	</div>
+</div>
+
+<div class="m-4">
+	<h1>Loading</h1>
+	<div class="align-center mt-5 flex gap-4">
+		<Button loading>Default</Button>
+		<Button loading variant="secondary">Secondary</Button>
+		<Button loading variant="transparent">Transparent</Button>
+		<Button loading variant="link">Link</Button>
+	</div>
+</div>
+
+<div class="m-4">
+	<h1>Disabled</h1>
+	<div class="align-center mt-5 flex gap-4">
+		<Button disabled>Default</Button>
+		<Button disabled variant="secondary">Secondary</Button>
+		<Button disabled variant="transparent">Transparent</Button>
+		<Button disabled variant="link">Link</Button>
+	</div>
+</div>
