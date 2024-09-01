d5a4ec6a059d51d01c4c12019f6147b897b2b17e
mohapakram <mohapmobile@gmail.com>
2024-09-01 20:21:04 +0300

    final bg color

--- apps/desktop/src/components/common/hoverableAction/HoverableAction.svelte
+++ apps/desktop/src/components/common/hoverableAction/HoverableAction.svelte
@@ -9,7 +9,7 @@
 <button
 	type="button"
 	on:click
-	class={`text-primary bg-secondary hover:bg-primary flex w-1/2 cursor-pointer flex-col items-center justify-center rounded-md p-2 text-center hover:text-white ${$$props.class}`}
+	class={`text-primary hover:bg-secondary flex w-1/2 cursor-pointer flex-col items-center justify-center rounded-md p-2 text-center ${$$props.class}`}
 >
 	<img src={imageSrc} class="w-full" alt="hoverable_action_image" />
 	<slot />
