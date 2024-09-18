150d4477a050ca7edfb8b9924e016e0d8ffa422a
farouqdaif1 <daiffarouq@gmail.com>
2024-09-18 22:37:52 +0300

    fix/remove hardcoded width

--- apps/desktop/src/routes/auth/local_sign_in/set_password_local_sign_in/+page.svelte
+++ apps/desktop/src/routes/auth/local_sign_in/set_password_local_sign_in/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex h-screen w-full flex-col items-center justify-center">
 	<Card
-		class="flex max-h-[340px] max-w-[600px] flex-col items-center justify-center bg-[#FBFBFB]  shadow-none "
+		class="flex max-h-[50%] max-w-[40%] flex-col items-center justify-center bg-[#FBFBFB]  shadow-none "
 	>
 		<div class="pb-14 pl-24 pr-24 pt-14">
 			<SetSignInLocalPassword />
